7aac741f06b328457952bf5fa0d63aa8586bb231
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-12-23 11:34:12 +0600

    book category section done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,10 +4,45 @@ import Sweeper from '../../components/Sweeper/Sweeper';
 const Home = () => {
     return (
         <div>
-            i am home 
-           <div className='mx-5 md:mx-10 '>
-           <Sweeper></Sweeper>
-           </div>
+
+            <div className='mx-5 md:mx-10 my-10'>
+                <Sweeper></Sweeper>
+            </div>
+            <div  className='my-10 mx-4 md:mx-10'>
+                <h1 className='text-blue-500 md:text-4xl font-bold text-3xl text-center mb-8'> Book Categories</h1>
+                <div className=' grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+                    <div className='text-center space-y-3 card  hover:scale-105 transition-all  bg-gray-200 shadow-xl'>
+                        <h1 className='text-2xl font-bold'>e.g., Novel</h1>
+                        <p className=''>
+                            A novel is a long fictional story with deep characters and themes, often in different genres. Examples include 1984 and Pride and Prejudice.</p>
+                    </div>
+
+                    <div className='text-center space-y-3 card hover:scale-105 transition-all bg-gray-200  shadow-xl'>
+                        <h1 className='text-2xl font-bold'> Thriller</h1>
+                        <p className=''>
+                            A thriller is a suspenseful genre focused on excitement, danger, and unexpected twists. Examples include The Girl with the Dragon Tattoo and The Silence of the Lambs.</p>
+                    </div>
+
+                    <div className='text-center space-y-3 card hover:scale-105 transition-all bg-gray-200  shadow-xl'>
+                        <h1 className='text-2xl font-bold'> History</h1>
+                        <p className=''>
+                        History is the study of past events, focusing on human actions, societies, and cultures. It helps us understand how the present world came to be, through examining records, artifacts, and stories from different periods.</p>
+                    </div>
+
+                    <div className='text-center space-y-3 card hover:scale-105 transition-all bg-gray-200  shadow-xl'>
+                        <h1 className='text-2xl font-bold'> Drama</h1>
+                        <p className=''>
+                        Drama is a genre of fiction that focuses on intense, emotional stories, often exploring conflicts and human experiences. It is typically performed on stage or screen.</p>
+                    </div>
+                    <div className='text-center space-y-3 card hover:scale-105 transition-all bg-gray-200  shadow-xl'>
+                        <h1 className='text-2xl font-bold'> Sci-Fi</h1>
+                        <p className=''>
+                        Sci-Fi (Science Fiction) is a genre that explores futuristic concepts, advanced technology, space exploration, and imaginative worlds, often set in the future or in alternative realities.</p>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     );
 };
