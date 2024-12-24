72c243f860b8ca85035e7476eaaa309bc1e9b697
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-12-24 12:52:07 +0600

    borrow book page data loade done

---
+++ src/components/BorrowedBookCard/BorrowedBookCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BorrowedBookCard = ({book}) => {
+    console.log(book)
+    return (
+        <div>
+            i ma borrowd book card
+        </div>
+    );
+};
+
+export default BorrowedBookCard;
--- src/pages/BorrowBooks/BorrowBooks.jsx
+++ src/pages/BorrowBooks/BorrowBooks.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
+import axios from 'axios';
+import BorrowedBookCard from '../../components/BorrowedBookCard/BorrowedBookCard';
 
 const BorrowBooks = () => {
+    const [books,setBooks]=useState([])
+    console.log(books)
+    const {user}=useAuth()
+    useEffect(()=>{
+        try{
+            axios.get(`${import.meta.env.VITE_URL}/borrow?email=${user.email}`)
+            .then(data=>{
+                setBooks(data.data)
+            })
+        }catch(error){console.log(error)}
+
+    },[user.email])
     return (
         <div>
-            i am borrow books
+            <h1 className='text-center text-3xl md:text-4xl font-bold my-8'>All Borrowed Books</h1>
+             <div>
+                {
+                    books.map(book=><BorrowedBookCard key={book._id} book={book}></BorrowedBookCard>)
+                }
+             </div>
         </div>
     );
 };
