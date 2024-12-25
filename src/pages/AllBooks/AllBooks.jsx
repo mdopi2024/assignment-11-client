a93b9e84fba33e369cbf4354fde98fcb0f9603e4
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-12-25 12:54:25 +0600

    show available book button done

--- src/pages/AllBooks/AllBooks.jsx
+++ src/pages/AllBooks/AllBooks.jsx
@@ -11,9 +11,17 @@ const AllBooks = () => {
         setAllbooks(data.data)
       })
     },[])
+
+    const handleAbailableBook=()=>{
+      const filter =allBooks.filter(book=>book.quantity !==0)
+      setAllbooks(filter)
+    }
     
     return (
         <div>
+          <div className='flex justify-end items-center px-10'>
+            <button onClick={handleAbailableBook} className='btn border-2 border-green-500 hover:bg-green-600 font-bold mt-5 bg-white '>Show Available Books </button>
+          </div>
           <div className='my-16 grid grid-cols-1 gap-8 md:grid-cols-3 lg:grid-cols-4   mx-5 md:mx-10'>
           {
             allBooks.map(book=><Bookscard key={book._id} book={book}></Bookscard>)
